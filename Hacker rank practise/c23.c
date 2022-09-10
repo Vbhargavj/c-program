73638eca351459e8f17d744536edc6ab13e6c2e5
bhargav <bhargavvadukar3@gmail.com>
2022-09-10 21:48:55 +0530

    i am add some new file there was created by me

---
+++ Hacker rank practise/c23.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int i, count = 0, count1 = 0, n, *ary;
+    scanf("%d", &n);
+    ary = (int *)malloc(n * sizeof(int));
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &ary[i]);
+        (ary[i] % 2 == 0) ? (count++) : (count1++);
+    }
+    (count1 < count) ? printf("READY FOR BATTLE") : printf("NOT READY\n");
+    return 0;
+}
