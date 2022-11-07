416f30dabbf2fa8f32239f28891500268513a45f
bhargav <bhargavvadukar3@gmail.com>
2022-11-08 01:02:07 +0530

    bhargav

--- DSA/short/4_insertation_sort.c
+++ DSA/short/4_insertation_sort.c
@@ -1,27 +1,39 @@
 #include <stdio.h>
 
-void insert_sort(int *ary, int n);
+void insert_sort(int ary[], int n)
+{
+    int temp, j, i;
+    for (i = 1; i < n; i++)
+    {
+        temp = ary[i];
+        j = i - 1;
+        while (j >= 0 && ary[j] > temp)
+        {
+            ary[j + 1] = ary[j];
+            j = j - 1;
+        }
+        ary[j + 1] = temp;
+    }
+}
+void prinet(int *ary, int n)
+{
+    printf("\n%d\n\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", ary[i]);
+    }
+}
 
 int main()
 {
     int ary[] = {35, 12, 5, 8, 13};
-    int n = 5;
-
-    insert_sort(ary, n);
-
+    int k = 5;
+    // fflush(stdout);
+    insert_sort(ary, k);
+    prinet(ary, k);
     return 0;
 }
-void insert_sort(int *ary, int n)
-{
-    // ary[1]=12 compare with 35
-    for (int i = 1; i < n; i++)
-    {
-        for (int j = i+1; j < n; j++)
-        {
-            if (ary[i] < ary[j])
-            {
-
-            }
-        }
-    }
-}
+/*
+    ary[] = {35, 12, 5, 8, 13};
+    35,| 12, 5, 8, 13  35>12
+*/
