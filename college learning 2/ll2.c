24c0d09072cb15d48a853db1bf3146d9f2501a6a
bhargav <bhargavvadukar3@gmail.com>
2022-11-22 18:54:11 +0530

    new modofication

--- college learning 2/ll2.c
+++ college learning 2/ll2.c
@@ -1,3 +1,4 @@
+// doubly linklist
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,10 +8,15 @@ struct node
     struct node *next;
     struct node *prev;
 };
-struct node *insertatfirst(struct node *, int);
+struct node *insert_at_first(struct node *, int);
 void insert_after_value(struct node *, int, int);
+void insert_before_value(struct node *, int, int);
 void insert_at_end(struct node *, int);
 void display(struct node *);
+struct node *delete_at_begin(struct node *);
+void delete_at_end(struct node *);
+void delete_at_value(struct node *);
+
 int main()
 {
     struct node *head = (struct node *)malloc(sizeof(struct node));
@@ -29,13 +35,19 @@ int main()
     last->data = 78;
     last->next = NULL;
 
-    display(head);
     // head = insertatfirst(head, 20);
-    insert_at_end(head, 23);
-    insert_after_value(head, 3, 50);
-
-    printf("\n");
+    // insert_at_end(head, 23);
+    // insert_at_end(head, 2);
+    // insert_at_end(head, 253);
+    // insert_at_end(head, 13);
+    // insert_after_value(head, 3, 50);
+    insert_before_value(head, 3, 12);
     display(head);
+    printf("\n");
+    // head = delete_at_begin(head);
+    // delete_at_end(head);
+    // delete_at_value(head);
+    // display(head);
 
     return 0;
 }
@@ -49,7 +61,7 @@ void display(struct node *ptr)
     }
 }
 
-struct node *insertatfirst(struct node *head, int val)
+struct node *insert_at_first(struct node *head, int val)
 {
     struct node *temp = (struct node *)malloc(sizeof(struct node));
     // struct node *head2=head;
@@ -59,6 +71,7 @@ struct node *insertatfirst(struct node *head, int val)
     temp->prev = NULL;
     head = temp;
 }
+
 void insert_at_end(struct node *head, int val)
 {
     int b = 0;
@@ -72,20 +85,18 @@ void insert_at_end(struct node *head, int val)
     temp->prev = head;
     head->next = temp;
 };
+
 void insert_after_value(struct node *head, int match, int val)
 {
-    printf("yes1\n");
     while (head->next != NULL)
     {
-        if (head->data != match)
+        if (head->data == match)
         {
             break;
         }
-
-        printf("yes\n");
         head = head->next;
     }
-    printf("yes3\n");
+
     if (head->data == match)
     {
         struct node *temp = (struct node *)malloc(sizeof(struct node));
@@ -94,5 +105,91 @@ void insert_after_value(struct node *head, int match, int val)
         temp->prev = head;
         head->next = temp;
         head->next->prev = temp;
+    }
+}
+
+// void insert_before_value(struct node *head, int match, int val)
+// {
+//     while (head->next != NULL)
+//     {
+//         if (head->next->data == match)
+//         {
+//             break;
+//         }
+//         head = head->next;
+//     }
+//     insert_after_value(head, head->data, val);
+// }
+
+void insert_before_value(struct node *head, int match, int val)
+{
+    while (head->next != NULL)
+    {
+        if (head->data == match)
+        {
+            break;
+        }
+        head = head->next;
+    }
+    if (head->data == match)
+    {
+        struct node *temp = (struct node *)malloc(sizeof(struct node));
+        temp->data = val;
+        temp->next = head;
+        temp->prev = head->prev;
+        head->prev = temp;
+        head->prev->prev->next= temp;
+        
+    }
+}
+
+struct node *delete_at_begin(struct node *head)
+{
+    struct node *temp = head;
+    head = head->next;
+    head->prev = NULL;
+    free(temp);
+    printf("Deleted\n");
+    return head;
+}
+void delete_at_end(struct node *head)
+{
+    // struct node *head2 = head;
+    while (head->next != NULL)
+    {
+        head = head->next;
+    }
+    struct node *temp = head;
+    free(temp);
+    head->prev->next = NULL;
+}
+
+void delete_at_value(struct node *head)
+{
+    int match;
+    printf("Enter the value");
+    scanf("%d", &match);
+
+    while (head->next != NULL)
+    {
+        // printf("in loop\n");
+        if (head->data == match)
+        {
+            // printf("in if\n");
+            break;
+        }
+        head = head->next;
+    }
+
+    if (head->data == match)
+    {
+        // printf("Yes\n");
+        struct node *temp = head;
+        // printf("Yes\n");
+        head->prev->next = head->next;
+        // printf("Yes\n");
+        head->next->prev = head->prev;
+        // printf("Yes\n");
+        free(temp);
     }
 }
